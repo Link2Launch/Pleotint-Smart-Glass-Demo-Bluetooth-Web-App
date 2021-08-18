4654a0a01942ed25f8bca93b7256dd64c9bd5d18
kakoijohn <kakoijohn@gmail.com>
2021-08-18 14:31:08 -0400

    Update app.js

--- web-app/js/app.js
+++ web-app/js/app.js
@@ -41,7 +41,7 @@ function connect() {
     }
     console.log('Requesting Bluetooth Device...');
     navigator.bluetooth.requestDevice({
-        filters: [{services: ['6E400001-B5A3-F393-­E0A9-­E50E24DCCA9E']}],
+        filters: [{services: ['0x0001']}],
         optionalServices: [bleNusServiceUUID],
         acceptAllDevices: false
     })
