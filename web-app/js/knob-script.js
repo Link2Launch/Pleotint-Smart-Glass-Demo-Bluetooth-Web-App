d4c8296dd4b9ad82ad4e085adb8f781271564b5f
kakoijohn <kakoijohn@gmail.com>
2021-08-20 14:06:39 -0400

    updated app ui to center

--- web-app/js/knob-script.js
+++ web-app/js/knob-script.js
@@ -1,4 +1,4 @@
-$(function($) {  
+$(function($) {
   $(".knob").knob({
     change : function (value) {
       //console.log("change : " + value);
@@ -6,6 +6,7 @@ $(function($) {
     release : function (value) {
       //console.log(this.$.attr('value'));
       console.log("release : " + value);
+      $('#st-data span').html(value);
     },
     cancel : function () {
       console.log("cancel : ", this);
@@ -50,32 +51,4 @@ $(function($) {
   }
 });
 
-// Example of infinite knob, iPod click wheel
-var v, up=0,down=0,i=0
-,$idir = $("div.idir")
-,$ival = $("div.ival")
-,incr = function() { i++; $idir.show().html("+").fadeOut(); $ival.html(i); }
-,decr = function() { i--; $idir.show().html("-").fadeOut(); $ival.html(i); };
-$("input.infinite").knob(
-  {
-    min : 0
-    , max : 20
-    , stopper : false
-    , change : function () {
-      if(v > this.cv){
-        if(up){
-          decr();
-          up=0;
-        }else{up=1;down=0;}
-      } else {
-        if(v < this.cv){
-          if(down){
-            incr();
-            down=0;
-          }else{down=1;up=0;}
-        }
-      }
-      v = this.cv;
-    }
-  });
 });
